8ff5b47d56501af11339a2ac1c09550d84a88106
wamballa <steve.tyrell@gmail.com>
2021-07-30 17:31:16 +0100

    cards used

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,85 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          Container(
-            child: Text('Hello'),
-          )
-        ],
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.red,
+              backgroundImage: new AssetImage('./images/avatar.jpg'),
+              radius: 50,
+            ),
+            Text(
+              'Mr Finger',
+              style: TextStyle(
+                fontFamily: 'Lobster',
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'Caveat',
+                fontStyle: FontStyle.normal,
+                color: Colors.teal.shade100,
+                fontSize: 20,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+              width: 150,
+              child: Divider(
+                color: Colors.teal.shade900,
+                // indent: 120,
+                // endIndent: 120,
+                // thickness: 0,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone_android,
+                  size: 40,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+44 7483 81 51 81',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.teal.shade900,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  size: 40,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'steve.tyrell@gmail.com',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.teal.shade900,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
